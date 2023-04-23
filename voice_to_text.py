8f033059c164699093788867668d06fa4e685ba9
Anand P S <56249313+Anand-PS@users.noreply.github.com>
2023-04-24 00:43:22 +0530

    Working

---
+++ voice_to_text.py
@@ -0,0 +1,23 @@
+from vosk import Model, KaldiRecognizer
+import pyaudio
+
+model = Model(r"/home/pi/Master/vosk-model-small-en-in-0.4")
+recognizer = KaldiRecognizer(model, 16000)
+mic = pyaudio.PyAudio()
+
+stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+stream.start_stream()
+
+while True:
+    try:
+        data = stream.read(4096)
+                
+        if recognizer.AcceptWaveform(data):
+            text = recognizer.Result()
+            response = text[14:-3]
+            if len(response) > 0:
+                print(response)
+            else:
+                print(".")
+    except OSError:
+        pass
